44c929c36d6bd436cd8b84bde22705dde5d618f4
Duy Ha <hacaoduy161@gmail.com>
2020-12-29 00:02:32 +0700

    first commit

---
+++ BTCN_06/src/main/java/BTCN_06/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BTCN_06;
+
+/**
+ *
+ * @author Windows
+ */
+public class Main {
+    public static String typeTriangle(float a, float b, float c) {
+        String msg = "";
+        if (a + b > c && b + c > a && a + c > b) {
+            if (a == b && b == c) {
+            msg = "Tam giác ??u";
+            }
+            else if (a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a+b*b) {
+                msg = "Tam giác vuông";
+            }
+            else if (a == b || b == c || c == a) {
+                msg = "Tam giác cân";
+            }
+            else {
+                msg = "Tam giác th??ng";
+            }
+        }
+        else {
+            msg = "Không ph?i tam giác";
+        }
+        return msg;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String result = typeTriangle(1,2,3);
+        System.out.print(result);
+    }
+    
+}
